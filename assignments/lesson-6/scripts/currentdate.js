106258a7260a08c603131126605e537c3cb90285
lindsayeyre <46468666+lindsayeyre@users.noreply.github.com>
2019-05-27 12:16:15 -0600

    update

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,35 +1,30 @@
-
-/*var today = new Date();
-var n = d.getDay();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-
-today = n + ',' + dd + mm + yyyy;
-document.getElementById('bar').innerHTML = today;*/
-
-/*var n = d.getDay();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-
-today = n + ',' + dd + mm + yyyy;*/
-var d = new Date();
-document.getElementById("nosestink").innerHTML = d.toDateString();
-
-
-
-
-/*var curday = function(sp){/
-    today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1; //As January is 0.
-    var yyyy = today.getFullYear();
-    
-    if(dd<10) dd='0'+dd;
-    if(mm<10) mm='0'+mm;
-    return (mm+sp+dd+sp+yyyy);
-    };
-    console.log(curday('/'));
-    console.log(curday('-'));
-    document.write(curday);*/
+/*var d = new Date();
+document.getElementById("footerbar").innerHTML = d.toDateString('en-US', {
+  weekday: 'long',
+  month: 'long'})*/
+  var newDate = new Date();
+  var day;
+  switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case  6:
+      day = "Saturday";
+  }
+  var datelett = A.getDate();
+  document.getElementById("footerbar").innerHTML = day + datelett;
